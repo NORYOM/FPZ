0a2618cf6846e788549b82fdaa84724c664621c8
NORYOM <noryom@163.com>
2019-10-15 21:47:01 +0800

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import svm
-from sklearn.decomposition import PCA
+from sklearn import manifold
 from sklearn.externals import joblib
 
 class Classify:
@@ -8,9 +8,11 @@ class Classify:
 	X = []	# 训练数据集
 	y = []	# 类型数据集
 	clsf = None
+	tsne = None
 
 	def  __init__(self):
 		self.clsf = svm.SVC(gamma='scale')
+		self.tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
 
 	def setTrainData(self,trainData):
 		for t in trainData:
@@ -20,15 +22,36 @@ class Classify:
 		for t in typeData:
 			self.y.append(t)
 
-	def train(self):
-		#### 待完成!!! self.X 需要降维处理，否则不能进行训练
-		#  1、尝试使用 svc = LinearSVC() 或者
-		#  2、先将 32*32 图形矩阵降维至 1*1024再分类
-		self.clsf.fit(self.X,self.y)
+	# 降维处理
+	def dimentionTransform(self,faceData):
+		newX = []
+		for x in faceData:
+			dim2X = self.tsne.fit_transform(x)
+			sum0 = 0
+			sum1 = 0
+			for nx in dim2X:
+				sum0 += nx[0]
+				sum1 += nx[1]
+			newX.append([sum0/len(dim2X),sum1/len(dim2X)])
+		return newX
 
-	def chkType(self,targetData):
-		arr = self.predict([targetData])
-		return arr[0]
+	def train(self):
+		if len(self.y)<2:
+			print("样本少于 2 个，请继续选择")
+			return
+		print("开始训练")
+
+		# 先将 32*32 图形矩阵降维至 2*32，然后计算两列的平均值之后再分类
+		newX = self.dimentionTransform(self.X)
+		self.clsf.fit(newX,self.y)
+
+	def chkType(self,faceData):
+		clsf = joblib.load('./train.mdl')
+		newFaces = self.dimentionTransform(faceData)
+		arr = clsf.predict(newFaces)
+		return arr
 
 	def saveModule(self):
+		print("开始保存模型")
 		joblib.dump(self.clsf,'./train.mdl')
+		print("保存结束")
